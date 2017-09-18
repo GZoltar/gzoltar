2bd8ba4b933c169b23c7aa96872d2a80df610e20
jose <zecapistolas@gmail.com>
2017-09-18 01:27:48 +0100

    Use maven-plugin API to prepare and start the GZoltar agent.

--- com.gzoltar.maven/src/main/java/com/gzoltar/maven/AgentMojo.java
+++ com.gzoltar.maven/src/main/java/com/gzoltar/maven/AgentMojo.java
@@ -1,16 +1,20 @@
 package com.gzoltar.maven;
 
 import java.io.File;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
-@Mojo(name = "init")
+@Mojo(name = "prepare-agent", defaultPhase = LifecyclePhase.INITIALIZE,
+    requiresDependencyResolution = ResolutionScope.RUNTIME, threadSafe = true)
 public class AgentMojo extends AbstractAgentMojo {
 
   /**
    * Path to the output file for execution data.
    */
-  @Parameter(property = "gzoltar.destFile", defaultValue = "${project.build.directory}/gzoltar.exec")
+  @Parameter(property = "gzoltar.destFile",
+      defaultValue = "${project.build.directory}/gzoltar.exec")
   private File destFile;
 
   /**
