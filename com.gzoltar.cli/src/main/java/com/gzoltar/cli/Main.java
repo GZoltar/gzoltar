ef8b59302258420a730af25f1a3cffc42eb81554
jose <zecapistolas@gmail.com>
2018-02-08 23:25:24 +0000

    Added command line interface module.

---
+++ com.gzoltar.cli/src/main/java/com/gzoltar/cli/Main.java
@@ -0,0 +1,24 @@
+package com.gzoltar.cli;
+
+public class Main {
+
+  /**
+   * Entry point of GZoltar execution
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    System.out.println("   ____ _____     _ _               \n" +
+                       "  / ___|__  /___ | | |_ __ _ _ __   \n" +
+                       " | |  _  / // _ \\| | __/ _` | '__| \n" +
+                       " | |_| |/ /| (_) | | || (_| | |     \n" +
+                       "  \\____/____\\___/|_|\\__\\__,_|_| \n");
+
+    // TODO
+
+    // make sure that everything ends here
+    System.exit(0);
+  }
+
+}
