9037172f1824110e52981e59b52b38439e235c12
jose <zecapistolas@gmail.com>
2019-05-08 22:57:06 -0700

    Added project example for GZoltar CLI.

---
+++ com.gzoltar.cli.examples/run.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+#
+# ------------------------------------------------------------------------------
+# This script performs fault-localization on a Java project using the GZoltar
+# command line interface either using instrumentation 'at runtime' or 'offline'.
+#
+# Usage:
+# ./run.sh
+#     --instrumentation <online|offline>
+#     [--help]
+#
+# Requirements:
+# - `java` and `javac` needs to be set and must point to the Java installation.
+#
+# ------------------------------------------------------------------------------
+
+SCRIPT_DIR=$(cd `dirname ${BASH_SOURCE[0]}` && pwd)
+
+#
+# Print error message and exit
+#
+die() {
+  echo "$@" >&2
+  exit 1
+}
+
+# ------------------------------------------------------------------ Envs & Args
+
+GZOLTAR_VERSION="1.7.2-SNAPSHOT"
+
+# Check whether GZOLTAR_CLI_JAR is set
+export GZOLTAR_CLI_JAR="$SCRIPT_DIR/../com.gzoltar.cli/target/com.gzoltar.cli-$GZOLTAR_VERSION-jar-with-dependencies.jar"
+[ "$GZOLTAR_CLI_JAR" != "" ] || die "GZOLTAR_CLI is not set!"
+[ -s "$GZOLTAR_CLI_JAR" ] || die "$GZOLTAR_CLI_JAR does not exist or it is empty! Please go to '$SCRIPT_DIR/..' and run 'mvn clean install'."
+
+# Check whether GZOLTAR_AGENT_RT_JAR is set
+export GZOLTAR_AGENT_RT_JAR="$SCRIPT_DIR/../com.gzoltar.agent.rt/target/com.gzoltar.agent.rt-$GZOLTAR_VERSION-all.jar"
+[ "$GZOLTAR_AGENT_RT_JAR" != "" ] || die "GZOLTAR_AGENT_RT_JAR is not set!"
+[ -s "$GZOLTAR_AGENT_RT_JAR" ] || die "$GZOLTAR_AGENT_RT_JAR does not exist or it is empty! Please go to '$SCRIPT_DIR/..' and run 'mvn clean install'."
+
+USAGE="Usage: ${BASH_SOURCE[0]} --instrumentation <online|offline> [--help]"
+if [ "$#" -eq "0" ]; then
+  die "$USAGE"
+fi
+mod_of_two=$(expr $# % 2)
+if [ "$#" -ne "1" ] && [ "$mod_of_two" -ne "0" ]; then
+  die "$USAGE"
+fi
+
+INSTRUMENTATION=""
+
+while [[ "$1" = --* ]]; do
+  OPTION=$1; shift
+  case $OPTION in
+    (--instrumentation)
+      INSTRUMENTATION=$1;
+      shift;;
+    (--help)
+      echo "$USAGE";
+      exit 0;;
+    (*)
+      die "$USAGE";;
+  esac
+done
+
+[ "$INSTRUMENTATION" != "" ] || die "$USAGE"
+if [ "$INSTRUMENTATION" != "online" ] && [ "$INSTRUMENTATION" != "offline" ]; then
+  die "$USAGE"
+fi
+
+LIB_DIR="$SCRIPT_DIR/lib"
+[ -d "$LIB_DIR" ] || die "$LIB_DIR does not exist!"
+
+JUNIT_JAR="$LIB_DIR/junit.jar"
+[ -s "$JUNIT_JAR" ] || die "$JUNIT_JAR does not exist or it is empty!"
+HAMCREST_JAR="$LIB_DIR/hamcrest-core.jar"
+[ -s "$HAMCREST_JAR" ] || die "$HAMCREST_JAR does not exist or it is empty!"
+
+BUILD_DIR="$SCRIPT_DIR/build"
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR" || die "Failed to create $BUILD_DIR!"
+
+SRC_DIR="$SCRIPT_DIR/src"
+TEST_DIR="$SCRIPT_DIR/test"
+
+# ------------------------------------------------------------------------- Main
+
+#
+# Compile
+#
+
+echo "Compile source and test cases ..."
+
+javac "$SRC_DIR/org/gzoltar/examples/CharacterCounter.java" -d "$BUILD_DIR" || die "Failed to compile source code!"
+javac -cp $JUNIT_JAR:$BUILD_DIR "$TEST_DIR/org/gzoltar/examples/CharacterCounterTest.java" -d "$BUILD_DIR" || die "Failed to compile test cases!"
+
+#
+# Collect list of unit test cases to run
+#
+
+echo "Collect list of unit test cases to run ..."
+
+UNIT_TESTS_FILE="$BUILD_DIR/tests.txt"
+
+java -cp $BUILD_DIR:$JUNIT_JAR:$HAMCREST_JAR:$GZOLTAR_CLI_JAR \
+  com.gzoltar.cli.Main listTestMethods $BUILD_DIR \
+    --outputFile "$UNIT_TESTS_FILE" \
+    --includes "org.gzoltar.examples.CharacterCounterTest#*" || die "Collection of unit test cases has failed!"
+[ -s "$UNIT_TESTS_FILE" ] || die "$UNIT_TESTS_FILE does not exist or it is empty!"
+
+#
+# Collect coverage
+#
+
+SER_FILE="$BUILD_DIR/gzoltar.ser"
+
+if [ "$INSTRUMENTATION" == "online" ]; then
+  echo "Perform instrumentation at runtime and run each unit test case in isolation ..."
+
+  # Perform instrumentation at runtime and run each unit test case in isolation
+  java -javaagent:$GZOLTAR_AGENT_RT_JAR=destfile=$SER_FILE,buildlocation=$BUILD_DIR,includes="org.gzoltar.examples.CharacterCounter:org.gzoltar.examples.CharacterCounter\$*",excludes="",inclnolocationclasses=false,output="file" \
+    -cp $BUILD_DIR:$JUNIT_JAR:$HAMCREST_JAR:$GZOLTAR_CLI_JAR \
+    com.gzoltar.cli.Main runTestMethods \
+      --testMethods "$UNIT_TESTS_FILE" \
+      --collectCoverage || die "Coverage collection has failed!"
+
+elif [ "$INSTRUMENTATION" == "offline" ]; then
+  echo "Perform offline instrumentation ..."
+
+  # Backup original classes
+  BUILD_BACKUP_DIR="$SCRIPT_DIR/.build"
+  mv "$BUILD_DIR" "$BUILD_BACKUP_DIR" || die "Backup of original classes has failed!"
+  mkdir -p "$BUILD_DIR"
+
+  # Perform offline instrumentation
+  java -cp $BUILD_BACKUP_DIR:$GZOLTAR_AGENT_RT_JAR:$GZOLTAR_CLI_JAR \
+    com.gzoltar.cli.Main instrument \
+    --outputDirectory "$BUILD_DIR" \
+    $BUILD_BACKUP_DIR || die "Offline instrumentation has failed!"
+
+  echo "Run each unit test case in isolation ..."
+
+  # Run each unit test case in isolation
+  java -cp $BUILD_DIR:$JUNIT_JAR:$HAMCREST_JAR:$GZOLTAR_AGENT_RT_JAR:$GZOLTAR_CLI_JAR \
+    -Dgzoltar-agent.destfile=$SER_FILE \
+    -Dgzoltar-agent.output="file" \
+    com.gzoltar.cli.Main runTestMethods \
+      --testMethods "$UNIT_TESTS_FILE" \
+      --offline \
+      --collectCoverage || die "Coverage collection has failed!"
+
+  # Restore original classes
+  cp -R $BUILD_BACKUP_DIR/* "$BUILD_DIR" || die "Restore of original classes has failed!"
+  rm -rf "$BUILD_BACKUP_DIR"
+fi
+
+[ -s "$SER_FILE" ] || die "$SER_FILE does not exist or it is empty!"
+
+#
+# Create fault localization report
+#
+
+echo "Create fault localization report ..."
+
+SPECTRA_FILE="$BUILD_DIR/sfl/txt/spectra.csv"
+MATRIX_FILE="$BUILD_DIR/sfl/txt/matrix.txt"
+TESTS_FILE="$BUILD_DIR/sfl/txt/tests.csv"
+
+java -cp $BUILD_DIR:$JUNIT_JAR:$HAMCREST_JAR:$GZOLTAR_CLI_JAR \
+  com.gzoltar.cli.Main faultLocalizationReport \
+    --buildLocation "$BUILD_DIR" \
+    --granularity "line" \
+    --inclPublicMethods \
+    --inclStaticConstructors \
+    --inclDeprecatedMethods \
+    --dataFile "$SER_FILE" \
+    --outputDirectory "$BUILD_DIR" \
+    --family "sfl" \
+    --formula "ochiai" \
+    --metric "entropy" \
+    --formatter "txt" || die "Generation of fault-localization report has failed!"
+
+[ -s "$SPECTRA_FILE" ] || die "$SPECTRA_FILE does not exist or it is empty!"
+[ -s "$MATRIX_FILE" ] || die "$MATRIX_FILE does not exist or it is empty!"
+[ -s "$TESTS_FILE" ] || die "$TESTS_FILE does not exist or it is empty!"
+
+echo "DONE!"
+exit 0
---
+++ com.gzoltar.cli.examples/test/org/gzoltar/examples/CharacterCounterTest.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C) 2019 GZoltar contributors.
+ * 
+ * This file is part of GZoltar.
+ * 
+ * GZoltar is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * GZoltar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with GZoltar. If
+ * not, see <https://www.gnu.org/licenses/>.
+ */
+package org.gzoltar.examples;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CharacterCounterTest {
+
+  @Test
+  public void test1() {
+    CharacterCounter cc = new CharacterCounter();
+    cc.processString("Al84");
+    assertEquals(2, cc.getNumLetters());
+    assertEquals(2, cc.getNumDigits());
+    assertEquals(0, cc.getNumOtherCharacters());
+  }
+
+  @Test
+  public void test2() {
+    CharacterCounter cc = new CharacterCounter();
+    cc.processString("1337");
+    assertEquals(0, cc.getNumLetters());
+    assertEquals(4, cc.getNumDigits());
+    assertEquals(0, cc.getNumOtherCharacters());
+  }
+
+  @Test
+  public void test3() {
+    CharacterCounter cc = new CharacterCounter();
+    cc.processString("T1");
+    assertEquals(1, cc.getNumLetters());
+    assertEquals(1, cc.getNumDigits());
+    assertEquals(0, cc.getNumOtherCharacters());
+  }
+
+  @Test
+  public void test4() {
+    CharacterCounter cc = new CharacterCounter();
+    cc.processString("UB40");
+    assertEquals(2, cc.getNumLetters());
+    assertEquals(2, cc.getNumDigits());
+    assertEquals(0, cc.getNumOtherCharacters());
+  }
+
+  @Test
+  public void test5() {
+    CharacterCounter cc = new CharacterCounter();
+    cc.processString("#lolcats");
+    assertEquals(7, cc.getNumLetters());
+    assertEquals(0, cc.getNumDigits());
+    assertEquals(1, cc.getNumOtherCharacters());
+  }
+
+  /*@Test
+  public void test6() {
+    CharacterCounter cc = new CharacterCounter();
+    cc.processString("\0x10\0x05\0x00");
+    assertEquals(0, cc.getNumLetters());
+    assertEquals(0, cc.getNumDigits());
+    assertEquals(3, cc.getNumOtherCharacters());
+  }*/
+
+  @Test
+  public void test7() {
+    CharacterCounter cc = new CharacterCounter();
+    cc.processString("GZoltar");
+    assertEquals(7, cc.getNumLetters());
+    assertEquals(0, cc.getNumDigits());
+    assertEquals(0, cc.getNumOtherCharacters());
+  }
+
+  @Test
+  public void test8() {
+    CharacterCounter cc = new CharacterCounter();
+    cc.processString("");
+    assertEquals(0, cc.getNumLetters());
+    assertEquals(0, cc.getNumDigits());
+    assertEquals(0, cc.getNumOtherCharacters());
+  }
+
+}
+
