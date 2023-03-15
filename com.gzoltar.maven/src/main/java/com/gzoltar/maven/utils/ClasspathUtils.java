1faf88bdb3f676b302ada21a0ced2342be93dbd6
David Fang <up202004179@edu.fe.up.pt>
2023-03-15 21:58:32 +0000

    ClassPathUtils '+1' removed and version update

--- com.gzoltar.maven/src/main/java/com/gzoltar/maven/utils/ClasspathUtils.java
+++ com.gzoltar.maven/src/main/java/com/gzoltar/maven/utils/ClasspathUtils.java
@@ -26,7 +26,7 @@ public final class ClasspathUtils {
 
   public static URL[] getTestClasspath(final MavenProject mavenProject) throws Exception {
     List<String> elements = mavenProject.getTestClasspathElements();
-    URL[] urls = new URL[elements.size() + 1];
+    URL[] urls = new URL[elements.size()];
     for (int i = 0; i < elements.size(); i++) {
       urls[i] = new File(elements.get(i)).toURI().toURL();
     }
